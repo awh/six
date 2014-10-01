e0053ac47a3a028d790b5d7be90cb881c1b734b7
Adam Harrison <harrisonadamw@gmail.com>
2014-10-01 11:46:20 +0100

    Streamlined function definitions

--- IPv6.hs
+++ IPv6.hs
@@ -21,7 +21,11 @@ base85Digits = ['0'..'9'] ++ ['A'..'Z'] ++ ['a'..'z'] ++ "!#$%&()*+-;<=>?@^_`{|}
 
 -- Smart constructor
 makeIPV6Address :: Word16 -> Word16 -> Word16 -> Word16 -> Word16 -> Word16 -> Word16 -> Word16 -> IPv6Address
-makeIPV6Address w0 w1 w2 w3 w4 w5 w6 w7 = IPv6Address (hexquadsToByteString w0 w1 w2 w3 w4 w5 w6 w7)
+makeIPV6Address w0 w1 w2 w3 w4 w5 w6 w7 =
+    IPv6Address (toLazyByteString (word16BE w0 <> word16BE w1 <>
+                                   word16BE w2 <> word16BE w3 <>
+                                   word16BE w4 <> word16BE w5 <>
+                                   word16BE w6 <> word16BE w7))
 
 -- Parse RFC 2373 textual address representation
 maybeIPv6Address :: String -> Maybe IPv6Address
@@ -37,20 +41,8 @@ showIPv6Address = show
 
 -- Show RFC 1924 compact address representation
 showCompactIPv6Address :: IPv6Address -> String
-showCompactIPv6Address ipv6Address = showIntAtBase 85 intToBase85Digit (ipv6AddressToInteger ipv6Address)  ""
-	where intToBase85Digit = (base85Digits!!)
-
--- Convert eight hexadecetets (Word16) to a ByteString
-hexquadsToByteString :: Word16 -> Word16 -> Word16 -> Word16 -> Word16 -> Word16 -> Word16 -> Word16 -> ByteString
-hexquadsToByteString w0 w1 w2 w3 w4 w5 w6 w7 =
-    toLazyByteString (word16BE w0 <> word16BE w1 <>
-                      word16BE w2 <> word16BE w3 <>
-                      word16BE w4 <> word16BE w5 <>
-                      word16BE w6 <> word16BE w7)
-
--- Interpret IPv6 address as a 128 bit unsigned integer
-ipv6AddressToInteger :: IPv6Address -> Integer
-ipv6AddressToInteger (IPv6Address bs) = Data.ByteString.Lazy.foldl op 0 bs
-    where op :: Integer -> Word8 -> Integer
-          op prev v = (prev * 256) + toInteger v
-
+showCompactIPv6Address (IPv6Address bs) = showIntAtBase 85 intToBase85Digit ipv6Integer ""
+    where intToBase85Digit = (base85Digits!!)
+          ipv6Integer = Data.ByteString.Lazy.foldl op 0 bs
+              where op :: Integer -> Word8 -> Integer
+                    op prev v = (prev * 256) + toInteger v
